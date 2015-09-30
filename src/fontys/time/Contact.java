e59d2d75ab744265750eb00fba561991f3b1db7e
GuusHamm <Dihw8vGH>
2015-09-30 15:29:46 +0200

    Contacts implented contacts, Made contact unit test with 100% coverage

--- src/fontys/time/Contact.java
+++ src/fontys/time/Contact.java
@@ -1,7 +1,48 @@
 package fontys.time;
 
+import com.sun.javafx.UnmodifiableArrayList;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
 /**
- * Created by Nekkyou on 30-9-2015.
+ * Created by linux on 30-9-2015.
  */
 public class Contact {
+
+    private String name;
+    private ArrayList appointments;
+
+    public Contact(String name) {
+        this.name = name;
+        appointments = new ArrayList();
+    }
+
+    public String getName(){
+
+        return name;
+    }
+
+    public Boolean addAppointment(Appointment a){
+
+        if (appointments.contains(a))   {
+            return false;
+        }
+        else {
+            appointments.add(a);
+            return true;
+        }
+    }
+
+    public void removeAppointment(Appointment a){
+        if (appointments.contains(a))   {
+            appointments.remove(a);
+        }
+    }
+
+    public List<Appointment> appointments(){
+        return Collections.unmodifiableList(appointments);
+    }
 }
