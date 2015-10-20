4b115f951a860cf7bfe38af8cd0efe1f80aceaa2
binq1000 <t.daniels@student.fontys.nl>
2015-10-20 12:43:36 +0200

    Started with Push

--- AEX-Banner/src/main/java/IBanner.java
+++ AEX-Banner/src/main/java/IBanner.java
@@ -1,11 +1,14 @@
-import jdk.nashorn.internal.runtime.PropertyListeners;
 
-import javax.swing.plaf.basic.BasicToolBarUI;
 import java.rmi.Remote;
+import java.util.ArrayList;
+
+import fontys.observer.RemotePropertyListener;
+import fontys.observer.RemotePublisher;
 
 /**
  * Created by xubuntu on 20-10-15.
  */
-public interface IBanner extends Remote implements PropertyListener {
-	public void setKoersen();
+public interface IBanner extends RemotePropertyListener, Remote
+{
+	public void setKoersen(ArrayList<IFonds> fondsen);
 }
