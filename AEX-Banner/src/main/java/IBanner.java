8121c1cc1091119df7fcc865f1f67d4fdff658cf
GuusHamm <guushamm@gmail.com>
2015-10-21 14:01:20 +0200

    Fixes Fixes everywhere

--- AEX-Banner/src/main/java/IBanner.java
+++ AEX-Banner/src/main/java/IBanner.java
@@ -1,11 +1,13 @@
 import jdk.nashorn.internal.runtime.PropertyListeners;
 
 import javax.swing.plaf.basic.BasicToolBarUI;
+
+import fontys.observer.RemotePropertyListener;
 import java.rmi.Remote;
 
 /**
  * Created by xubuntu on 20-10-15.
  */
-public interface IBanner extends Remote implements PropertyListener {
+public interface IBanner extends Remote, RemotePropertyListener {
 	public void setKoersen();
 }
